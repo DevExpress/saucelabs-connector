28babffe713c7cf797c41e92fa8c0911d42190e6
Farfurix <vladimir.airich@gmail.com>
2021-03-31 12:11:05 +0300

    Correct messages

--- src/messages.js
+++ src/messages.js
@@ -1,10 +1,10 @@
 export const MESSAGE = {
     noFreeMachines:                        'There are no free machines',
-    failedToCallSauceApi:                  'Sauce Labs API request failed.\n{err}',
+    failedToCallSauceApi:                  'Failed to reach the Sauce Labs API.\n{err}',
     unexpectedSauceApiResponse:            'Unexpected response from Sauce Labs.\n{method} {url}\nResponse status: {statusCode}\nBody: {body}',
     freeMachinesNumberIsLessThanRequested: 'The number of free machines ({freeMachineCount}) is less than requested ({machineCount}).',
     browserStarted:                        '{browser.browserName} started. See {sessionUrl}',
-    failedToReadIePrerunBat:               'Failed to read the Internet Explorer 11 prerun file ({filePath}{fileName}).\n{err}'
+    failedToReadIePrerunBat:               'Failed to read the Internet Explorer 11 pre-run executable ({filePath}{fileName}).\n{err}'
 };
 
 export function getText (template, parameters) {
